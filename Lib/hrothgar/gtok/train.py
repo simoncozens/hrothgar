797444b87323268d2fbf644bb5f46807263a999b
Simon Cozens <simon@simon-cozens.org>
2026-06-24 12:04:43 +0000

    Go back to what worked and establish a baseline

--- Lib/hrothgar/gtok/train.py
+++ Lib/hrothgar/gtok/train.py
@@ -310,7 +310,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max-display-score",
         type=int,
-        default=100,
+        default=50,
         help=(
             "Exclude fonts with display_score() above this threshold. "
             "Display fonts have extreme stylistic variation that a shared "
