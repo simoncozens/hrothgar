c822e986205696d9cd751735391953c5a7d5e10f
Simon Cozens <simon@simon-cozens.org>
2026-06-30 13:52:23 +0000

    Try smaller gtok

--- Lib/hrothgar/gtok/config.py
+++ Lib/hrothgar/gtok/config.py
@@ -28,10 +28,10 @@ class GtokConfig:
     vit_attention_dropout: float = 0.2
 
     # Quantization parameters
-    quantizer_codebook_size: int = 16384  # Size of the codebook
+    quantizer_codebook_size: int = 8192  # Size of the codebook
     quantizer_beta: float = 0.25  # Commitment loss weight
     quantizer_entropy_loss_ratio: float = 0.2  # Entropy regularization weight
-    quantizer_ema_decay: float = 0.99
+    quantizer_ema_decay: float = 0.95
 
     def __post_init__(self):
         """Set defaults for list parameters."""
