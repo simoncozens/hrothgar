667ac1058508c87ce3cbb7a3d648143dca2f934b
Simon Cozens <simon@simon-cozens.org>
2026-06-30 14:24:20 +0000

    Revert "Tweak parameters"
    This reverts commit 114ae9058770bbaad8bff4aab7a9cfad7c11853e.

--- Lib/hrothgar/gtok/train.py
+++ Lib/hrothgar/gtok/train.py
@@ -60,7 +60,7 @@ class GtokTrainingLoop(TrainingLoop):
             class_balanced=True,
             max_display_score=train_args.max_display_score,
             gtok_config=config,
-            render_time_augmentation=True,
+            render_time_augmentation=False,
         )
         self._maker = maker
         self.optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
